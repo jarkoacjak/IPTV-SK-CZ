33742869667d84a9aa25425372d66d49e3f3be2f
jarkoacjak <videojarkohrarobloxacjak@gmail.com>
2026-02-23 11:34:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,38 +4,34 @@ import urllib.parse
 import xbmcgui
 import xbmcplugin
 
+_url = sys.argv[0]
 _handle = int(sys.argv[1])
 
-def main_menu():
-    # Kategória Slovensko
-    li_sk = xbmcgui.ListItem(label='[B]SLOVENSKÉ STANICE[/B]')
-    url_sk = sys.argv[0] + '?' + urllib.parse.urlencode({'mode': 'sk'})
-    xbmcplugin.addDirectoryItem(handle=_handle, url=url_sk, listitem=li_sk, isFolder=True)
+def get_url(**kwargs):
+    return _url + '?' + urllib.parse.urlencode(kwargs)
 
-    # Kategória Česko
-    li_cz = xbmcgui.ListItem(label='[B]ČESKÉ STANICE[/B]')
-    url_cz = sys.argv[0] + '?' + urllib.parse.urlencode({'mode': 'cz'})
-    xbmcplugin.addDirectoryItem(handle=_handle, url=url_cz, listitem=li_cz, isFolder=True)
-    
+def main_menu():
+    # Slovensko
+    li = xbmcgui.ListItem(label='[B]Slovenské TV[/B]')
+    xbmcplugin.addDirectoryItem(handle=_handle, url=get_url(action='sk'), listitem=li, isFolder=True)
+    # Česko
+    li = xbmcgui.ListItem(label='[B]České TV[/B]')
+    xbmcplugin.addDirectoryItem(handle=_handle, url=get_url(action='cz'), listitem=li, isFolder=True)
     xbmcplugin.endOfDirectory(_handle)
 
 def list_sk():
-    # TV JOJ Link
     url = "https://live.cdn.joj.sk/live/andromeda/joj-1080.m3u8"
     headers = "|User-Agent=Mozilla/5.0&Referer=https://www.joj.sk/"
-    
     li = xbmcgui.ListItem(label='TV JOJ')
-    li.setInfo('video', {'title': 'TV JOJ'})
+    li.setInfo('video', {'title': 'TV JOJ Live'})
     li.setProperty('IsPlayable', 'true')
     xbmcplugin.addDirectoryItem(handle=_handle, url=url + headers, listitem=li, isFolder=False)
     xbmcplugin.endOfDirectory(_handle)
 
-# Spúšťacia logika
-params = dict(urllib.parse.parse_qsl(sys.argv[2][1:]))
-mode = params.get('mode')
-
-if mode is None:
-    main_menu()
-elif mode == 'sk':
-    list_sk()
-    
+if __name__ == '__main__':
+    params = dict(urllib.parse.parse_qsl(sys.argv[2][1:]))
+    if not params:
+        main_menu()
+    elif params.get('action') == 'sk':
+        list_sk()
+        
