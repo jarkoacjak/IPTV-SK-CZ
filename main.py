1d52d88c8e1e146444f9f9269baac10b8e815bc4
jarkoacjak <videojarkohrarobloxacjak@gmail.com>
2026-02-23 11:18:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,69 +1,62 @@
+# -*- coding: utf-8 -*-
 import sys
 import urllib.parse
 import xbmcgui
 import xbmcplugin
 
-# 1. Nastavenia doplnku
+# 1. Získanie ID a URL doplnku
 HANDLE = int(sys.argv[1])
 BASE_URL = sys.argv[0]
 
 def build_url(query):
+    """Pomocná funkcia na vytváranie odkazov v menu"""
     return BASE_URL + '?' + urllib.parse.urlencode(query)
 
 def main_menu():
-    """Vytvorí hlavné menu: Slovenské a České"""
+    """Hlavná obrazovka: Výber Slovensko alebo Česko"""
     # SLOVENSKO
+    li_sk = xbmcgui.ListItem(label='[B]SLOVENSKÉ STANICE[/B]')
     url_sk = build_url({'mode': 'list_sk'})
-    li_sk = xbmcgui.ListItem(label='[COLOR yellow][B]Slovenské TV[/B][/COLOR]')
     xbmcplugin.addDirectoryItem(handle=HANDLE, url=url_sk, listitem=li_sk, isFolder=True)
 
     # ČESKO
+    li_cz = xbmcgui.ListItem(label='[B]ČESKÉ STANICE[/B]')
     url_cz = build_url({'mode': 'list_cz'})
-    li_cz = xbmcgui.ListItem(label='[COLOR blue][B]České TV[/B][/COLOR]')
     xbmcplugin.addDirectoryItem(handle=HANDLE, url=url_cz, listitem=li_cz, isFolder=True)
 
     xbmcplugin.endOfDirectory(HANDLE)
 
 def list_slovak_tv():
     """Zoznam slovenských staníc"""
-    # TV JOJ (tvoj link)
-    url_joj = "https://live.cdn.joj.sk/live/andromeda/joj-1080.m3u8"
-    headers = "|User-Agent=Mozilla/5.0&Referer=https://www.joj.sk/"
+    # TV JOJ
+    joj_url = "https://live.cdn.joj.sk/live/andromeda/joj-1080.m3u8"
+    # Dôležité hlavičky pre Jojku
+    headers = "|User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) &Referer=https://www.joj.sk/"
     
     li = xbmcgui.ListItem(label='TV JOJ')
     li.setInfo('video', {'title': 'TV JOJ Live'})
     li.setProperty('IsPlayable', 'true')
-    # Nastavenie ikony (voliteľné)
-    li.setArt({'icon': 'https://upload.wikimedia.org/wikipedia/commons/4/4b/TV_JOJ_logo_2015.png'})
+    # Pridanie loga (voliteľné)
+    li.setArt({'thumb': 'https://upload.wikimedia.org/wikipedia/commons/4/4b/TV_JOJ_logo_2015.png'})
     
-    xbmcplugin.addDirectoryItem(handle=HANDLE, url=url_joj + headers, listitem=li, isFolder=False)
+    xbmcplugin.addDirectoryItem(handle=HANDLE, url=joj_url + headers, listitem=li, isFolder=False)
+    
     xbmcplugin.endOfDirectory(HANDLE)
 
 def list_czech_tv():
-    """Zoznam českých staníc"""
-    # Tu si môžeš pridať ďalšie stanice neskôr
-    li = xbmcgui.ListItem(label='ČT1 (Testovací link)')
-    li.setInfo('video', {'title': 'ČT1'})
-    li.setProperty('IsPlayable', 'true')
-    
-    # Príklad cesty, ak by si mal iný link
-    test_url = "http://priklad.com/test.m3u8"
-    
-    xbmcplugin.addDirectoryItem(handle=HANDLE, url=test_url, listitem=li, isFolder=False)
+    """Zoznam českých staníc (zatiaľ prázdny)"""
+    li = xbmcgui.ListItem(label='[I]Tu zatiaľ nič nie je[/I]')
+    xbmcplugin.addDirectoryItem(handle=HANDLE, url='', listitem=li, isFolder=False)
     xbmcplugin.endOfDirectory(HANDLE)
 
-# --- LOGIKA SPÚŠŤANIA ---
-# Rozoberie URL parametre, aby doplnok vedel, čo má zobraziť
+# 2. Logika prepínania medzi menu (Routing)
 params = dict(urllib.parse.parse_qsl(sys.argv[2][1:]))
 mode = params.get('mode')
 
 if mode is None:
-    # Ak sme práve otvorili doplnok, ukáž hlavné menu
     main_menu()
 elif mode == 'list_sk':
-    # Ak používateľ klikol na Slovenské TV
     list_slovak_tv()
 elif mode == 'list_cz':
-    # Ak používateľ klikol na České TV
     list_czech_tv()
     
